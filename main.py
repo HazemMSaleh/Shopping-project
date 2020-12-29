ed0e0859c95a7a3d63fc539d96afa9537aa200b7
HazemMSaleh <76664511+HazemMSaleh@users.noreply.github.com>
2020-12-28 22:24:40 -0600

    Created database and allowed the program to view it through /viewer using the viewer.html template

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import sqlite3 as sql
 app = Flask(__name__)
 app.secret_key = 'development key'
 @app.route('/')
 def login():
    return render_template('homepage.html')
 
+@app.route('/viewer')
+def viewer():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   cur = con.cursor()
+   cur.execute("select * from orders")
+   rows = cur.fetchall();
+   return render_template('viewer.html', rows = rows)
+
 if __name__ == '__main__':
    app.run(debug = True)
+
 
