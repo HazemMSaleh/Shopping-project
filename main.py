b17149244b71bd6ddcc71b7eb33c644f0edd857a
HazemMSaleh <76664511+HazemMSaleh@users.noreply.github.com>
2021-01-01 21:25:15 -0600

    Added ability to add new orders to database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3 as sql
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'development key'
@@ -25,11 +26,15 @@ def viewer():
     if date_opened == '':
         date_opened = None
     date_fulfilled = request.form['date_fulfilled']
-    order_status = request.form ['order_status']
+    try:
+        order_status = request.form['order_status']
+    except Exception:
+        order_status = ''
     if date_fulfilled == '' and order_status == 'open':
         date_fulfilled = 'IS NULL'
     elif date_fulfilled == '':
         date_fulfilled = None
+    print(order_status)
     frequent_item = request.form['frequent_item']
     if frequent_item == 'no_filter':
         frequent_item = None
@@ -84,6 +89,33 @@ def viewer():
     cur.execute(sql_statement)
     rows = cur.fetchall()
     return render_template('viewerresults.html', rows=rows)
+    con.close()
+
+
+@app.route('/editorhome')
+def editorhome():
+    return render_template('editorhome.html')
+
+
+@app.route('/neworder', methods=['GET', 'POST'])
+def neworder():
+    if request.method == 'POST':
+        con = sql.connect("database.db")
+        cur = con.cursor()
+        requestor = request.form['user']
+        cur.execute('SELECT id from users where user = (?)', [requestor])
+        requestor_id = cur.fetchone()[0]
+        requested_item = request.form['frequent_item']
+        cur.execute('SELECT id from frequent_items where name = (?)', [requested_item])
+        requested_item_id = cur.fetchone()[0]
+        date_requested_unformatted = datetime.datetime.now()
+        date_requested = date_requested_unformatted.strftime("%x")
+        cur.execute('INSERT INTO orders (date_opened, user_id, frequent_item_id) VALUES ((?),(?),(?))', (date_requested, requestor_id, requested_item_id))
+        con.commit()
+        return render_template('neworder.html')
+
+    else:
+        return render_template('neworder.html')
 
 
 if __name__ == '__main__':
